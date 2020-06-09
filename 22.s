2afcccfe691d8bbd33658e99a0b79673f3d496ae
Andjela Niketic <andjelaniketic109@gmail.com>
2020-06-10 00:42:34 +0200

    swap (pokazivaci i nizovi u asembleru)

---
+++ 22.s
@@ -0,0 +1,18 @@
+.intel_syntax noprefix
+.text
+.global zameni
+
+#void zameni(int*, int*)
+#			 rdi	rsi
+
+zameni:
+enter 0,0
+
+mov eax, [rdi]
+mov ecx, [rsi]
+
+mov [rdi], ecx
+mov [rsi], eax
+
+leave
+ret
